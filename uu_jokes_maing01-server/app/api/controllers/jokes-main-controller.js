7ac389532f3b5951aea25310d4d7fd80a2f52dfa
Daryna Nikolaienko <1788-1691-1@plus4u.net>
2021-07-04 19:28:06 +0300

    Add joke-controller.js and joke-main-controller.js

--- uu_jokes_maing01-server/app/api/controllers/jokes-main-controller.js
+++ uu_jokes_maing01-server/app/api/controllers/jokes-main-controller.js
@@ -1,14 +1,54 @@
 "use strict";
-const JokesMainAbl = require("../../abl/jokes-main-abl.js");
 
-class JokesMainController {
+const JokesInstanceAbl = require("../../abl/jokes-instance-abl.js");
 
-  jokeCreate(ucEnv) {
-    return JokesMainAbl.jokeCreate(ucEnv.getUri().getAwid(), ucEnv.getDtoIn());
+// const CACHE_VALUE = "public, max-age=86400, s-maxage=86400";
+
+class JokesInstanceController {
+  static init(ucEnv) {
+    return JokesInstanceAbl.init(ucEnv.getUri(), ucEnv.getDtoIn(), ucEnv.getSession());
   }
-  init(ucEnv) {
-    return JokesMainAbl.init(ucEnv.getUri(), ucEnv.getDtoIn(), ucEnv.getSession());
+
+  // static plugInBt(ucEnv) {
+  //   return JokesInstanceAbl.plugInBt(ucEnv.getUri(), ucEnv.getDtoIn(), ucEnv.getSession());
+  // }
+  // static load(ucEnv) {
+  //   return JokesInstanceAbl.load(ucEnv.uri.getAwid(), ucEnv.getAuthorizationResult());
+  // }
+
+  static update(ucEnv) {
+    return JokesInstanceAbl.update(ucEnv.uri.getAwid(), ucEnv.parameters);
   }
+
+  // static setLogo(ucEnv) {
+  //   return JokesInstanceAbl.setLogo(ucEnv.uri.getAwid(), ucEnv.parameters);
+  // }
+  //
+  // static setIcons(ucEnv) {
+  //   return JokesInstanceAbl.setIcons(ucEnv.uri.getAwid(), ucEnv.parameters, ucEnv.getUri());
+  // }
+
+  static getProductInfo(ucEnv) {
+    ucEnv.getResponse().setHeaders({ "Cache-Control": CACHE_VALUE });
+    return JokesInstanceAbl.getProductInfo(ucEnv.uri.getAwid());
+  }
+
+  // static async getProductLogo(ucEnv) {
+  //   let dtoOut = await JokesInstanceAbl.getProductLogo(ucEnv.getUri().getAwid(), ucEnv.parameters);
+  //   ucEnv.getResponse().setHeaders({ "Cache-Control": CACHE_VALUE });
+  //   return ucEnv.setBinaryDtoOut(dtoOut);
+  // }
+  //
+  // static async getUveMetaData(ucEnv) {
+  //   let dtoOut = await JokesInstanceAbl.getUveMetaData(ucEnv.getUri().getAwid(), ucEnv.parameters);
+  //   ucEnv.getResponse().setHeaders({ "Cache-Control": CACHE_VALUE });
+  //   return ucEnv.setBinaryDtoOut(dtoOut);
+  // }
+  //
+  // static async getIndex(ucEnv) {
+  //   ucEnv.getResponse().setHeaders({ "Cache-Control": CACHE_VALUE });
+  //   return JokesInstanceAbl.getIndex(ucEnv.uri.getAwid(), ucEnv.getUri());
+  // }
 }
 
-module.exports = new JokesMainController();
+module.exports = JokesInstanceController;
